49716641080c55892b9213ca2edf13ae159ffee3
KonstantinEger <35273325+KonstantinEger@users.noreply.github.com>
2020-11-04 22:02:30 +0100

    feed forward algorithm

--- src/neural_net/mod.rs
+++ src/neural_net/mod.rs
@@ -1,4 +1,5 @@
 use wasm_bindgen::prelude::wasm_bindgen;
+use std::convert::TryInto;
 use crate::Matrix;
 
 /// An instance of NeuralNet is able to perform calculations on some
@@ -57,6 +58,41 @@ impl NeuralNet
 			hidden_weights
 		}
 	}
+
+	pub fn feed_forward(&self, input_data: Vec<f64>) -> Vec<f64>
+	{
+		let inputs = Matrix::from(input_data.len().try_into().unwrap(), 1, input_data).unwrap();
+		let mut weights_iter = self.hidden_weights.iter();
+		
+		let mut hidden = Matrix::mult(&weights_iter.next().unwrap(), &inputs).unwrap();
+		hidden.map(|v, _, _| v + (self.bias as f64));
+		hidden.map(|v, _, _| activation_func::sigmoid(v));
+
+		if self.hidden_nodes.len() == 0 {
+			return hidden.data();
+		}
+
+		for weights in weights_iter {
+			hidden = Matrix::mult(weights, &hidden).unwrap();
+			hidden.map(|v, _, _| v + (self.bias as f64));
+			hidden.map(|v, _, _| activation_func::sigmoid(v));
+		}
+
+		hidden.data()
+	}
+}
+
+mod activation_func
+{
+	pub fn sigmoid(x: f64) -> f64
+	{
+		1. / (1. + (-x).exp())
+	}
+
+	pub fn dsigmoid(y: f64) -> f64
+	{
+		y * (1. - y)
+	}
 }
 
 #[cfg(test)]
@@ -76,5 +112,33 @@ mod tests
 	{
 		let nn = NeuralNet::new(2, Vec::new(), 1);
 		assert_eq!(nn.hidden_weights.len(), 1);
+	}
+
+	#[test]
+	fn feed_forward() {
+		let nn = NeuralNet::new(2, vec![3, 4], 2);
+		let result = nn.feed_forward(vec![1., 1.]);
+		assert_eq!(result.len(), 2);
+	}
+
+	#[test]
+	fn sigmoid()
+	{
+		let of_0 = super::activation_func::sigmoid(0.);
+		let of_1 = super::activation_func::sigmoid(1.);
+		assert_eq!(of_0, 0.5);
+		assert!(of_1 >= 0.7310 && of_1 <= 0.7311);
+	}
+
+	#[test]
+	fn dsigmoid()
+	{
+		let of_0 = super::activation_func::dsigmoid(0.);
+		let of_0_5 = super::activation_func::dsigmoid(0.5);
+		let of_1 = super::activation_func::dsigmoid(1.);
+
+		assert_eq!(of_0, 0.);
+		assert_eq!(of_0_5, 0.25);
+		assert_eq!(of_1, 0.);
 	}
 }
